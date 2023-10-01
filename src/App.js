e4a6daf6fce43cc4467cf0cda72e22d8f3eeb9eb
Eduardo Eli <eduardoely@live.com>
2023-10-01 19:59:14 -0300

    HashRouter to work in gh pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import NotFoundPage from "./pages/notFoundPage";
 
 export default function App() {
   return (
-    <Router basename="frontend-doador-online">
+    <HashRouter>
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="/login" element={<>Login</>} />
+        <Route path="/cadastro" element={<>Cadastro</>} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
